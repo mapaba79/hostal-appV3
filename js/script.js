694320eca418b5edf20f6ba12e7e1f3e0ca414e3
mapaba79 <mateuspaimdebarros23121997@gmail.com>
2025-05-20 22:26:15 -0300

    Fixing formatting.

--- js/script.js
+++ js/script.js
@@ -13,8 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 4, nome: 'Quarto Privado 4', tipo: 'privado' },
         { id: 5, nome: 'Quarto Privado 5', tipo: 'privado' },
         { id: 6, nome: 'Quarto Privado 6', tipo: 'privado' },
-        { id: 7, nome: 'Quarto Compartilhado', tipo: 'compartilhado', camas: ['1s', '1m', '1i', '2s', '2m', '2i', '3s', '3m', '3i', '4s', '4m', '4i', '5s', '5m', '5i', '6s', '6m', '6i'] },
-        { id: 8, nome: 'Quarto dos Voluntários', tipo: 'voluntarios', camas: ['1s', '1m', '1i', '2s', '2m', '2i', '3s', '3m', '3i'] }
+        { id: 7, nome: 'Quarto Compartilhado', tipo: 'compartilhado', camas: ['1s', 
+            '1m', '1i', '2s', '2m', '2i', '3s', '3m', '3i', '4s', '4m', '4i', '5s', 
+            '5m', '5i', '6s', '6m', '6i'] },
+        { id: 8, nome: 'Quarto dos Voluntários', tipo: 'voluntarios', camas: ['1s', 
+            '1m', '1i', '2s', '2m', '2i', '3s', '3m', '3i'] }
     ];
 
     const listaReservas = document.getElementById('lista-reservas');
@@ -50,7 +53,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const telefone = document.getElementById('telefone-hospede').value;
         const nacionalidade = document.getElementById('nacionalidade-hospede').value;
         const documento = document.getElementById('documento-hospede').value;
-        const novoHospede = { id: Date.now(), nome, email, telefone, nacionalidade, documento };
+        const novoHospede = { id: Date.now(), nome, email, telefone, 
+            nacionalidade, documento };
         hospedes.push(novoHospede);
         salvarHospedes();
         renderizarHospedes();
@@ -83,7 +87,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function renderizarOpcoesHospedes() {
-        hospedeReservaSelect.innerHTML = '<option value="" disabled selected>Selecione um hóspede</option>';
+        hospedeReservaSelect.innerHTML = 
+            '<option value="" disabled selected>Selecione um hóspede</option>';
         hospedes.forEach(hospede => {
             const option = document.createElement('option');
             option.value = hospede.id;
@@ -102,8 +107,11 @@ document.addEventListener('DOMContentLoaded', () => {
             if (quarto.tipo === 'privado' && isQuartoOcupado(quarto.nome)) {
                 li.innerHTML = `<span class="ocupado">${quarto.nome} (Ocupado)</span>`;
             } else if (quarto.camas) {
-                const camasOcupadas = reservas.filter(reserva => reserva.quarto === quarto.nome).map(reserva => reserva.quartoCama ? reserva.quartoCama.split('-')[1] : null).filter(cama => cama);
-                const camasLivres = quarto.camas.filter(cama => !camasOcupadas.includes(cama));
+                const camasOcupadas = reservas.filter(reserva => reserva.quarto === 
+                    quarto.nome).map(reserva => reserva.quartoCama ? 
+                    reserva.quartoCama.split('-')[1] : null).filter(cama => cama);
+                const camasLivres = quarto.camas.filter(cama => 
+                    !camasOcupadas.includes(cama));
 
                 const spanCamas = document.createElement('span');
                 spanCamas.style.display = 'block';
@@ -135,7 +143,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function isQuartoOcupado(nomeQuarto) {
-        return reservas.some(reserva => reserva.quarto === nomeQuarto && !reserva.quartoCama);
+        return reservas.some(reserva => reserva.quarto === 
+            nomeQuarto && !reserva.quartoCama);
     }
 
     // --- Funcionalidades para Reservas ---
@@ -151,10 +160,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const quartoId = parseInt(quartoReservaSelect.value);
         const quartoSelecionado = quartos.find(q => q.id === quartoId);
 
-        if (quartoSelecionado && (quartoSelecionado.tipo === 'compartilhado' || quartoSelecionado.tipo === 'voluntarios')) {
+        if (quartoSelecionado && (quartoSelecionado.tipo === 'compartilhado' || 
+            quartoSelecionado.tipo === 'voluntarios')) {
             camasContainer.style.display = 'block';
-            camaReservaSelect.innerHTML = '<option value="" disabled selected>Selecione uma cama</option>';
-            const camasOcupadasNoQuarto = reservas.filter(reserva => reserva.quarto === quartoSelecionado.nome).map(reserva => reserva.quartoCama ? reserva.quartoCama.split('-')[1] : null).filter(cama => cama);
+            camaReservaSelect.innerHTML = 
+                '<option value="" disabled selected>Selecione uma cama</option>';
+            const camasOcupadasNoQuarto = reservas.filter(reserva => 
+                reserva.quarto === quartoSelecionado.nome).map(reserva => 
+                reserva.quartoCama ? reserva.quartoCama.split('-')[1] : 
+                    null).filter(cama => cama);
 
             quartoSelecionado.camas.forEach(cama => {
                 const option = document.createElement('option');
@@ -173,9 +187,12 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
         const hospedeId = parseInt(hospedeReservaSelect.value);
         const quartoId = parseInt(quartoReservaSelect.value);
+        const valorPago = document.getElementById('valor-pago').value ? 
+            parseFloat(document.getElementById('valor-pago').value) : 0;
         const dataCheckin = document.getElementById('data-checkin').value;
         const dataCheckout = document.getElementById('data-checkout').value;
-        const camaSelecionada = camasContainer.style.display === 'block' ? camaReservaSelect.value : null;
+        const camaSelecionada = camasContainer.style.display === 'block' ? 
+            camaReservaSelect.value : null;
 
         const hospede = hospedes.find(h => h.id === hospedeId);
         const quarto = quartos.find(q => q.id === quartoId);
@@ -187,7 +204,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 quarto: quarto.nome,
                 quartoCama: camaSelecionada,
                 checkin: dataCheckin,
-                checkout: dataCheckout
+                checkout: dataCheckout,
+                valorPago: valorPago
             };
             reservas.push(novaReserva);
             salvarReservas();
@@ -205,11 +223,13 @@ document.addEventListener('DOMContentLoaded', () => {
         listaReservas.innerHTML = '';
         reservas.forEach((reserva, index) => {
             const li = document.createElement('li');
-            let detalhesReserva = `${reserva.hospede} - ${reserva.quarto}`;
+            let detalhesReserva = `${reserva.hospede} | ${reserva.quarto}`;
             if (reserva.quartoCama) {
                 detalhesReserva += ` (Cama: ${reserva.quartoCama.split('-')[1]})`;
             }
-            detalhesReserva += ` (${reserva.checkin} até ${reserva.checkout})`;
+            detalhesReserva += ` - (${reserva.checkin} - ${reserva.checkout})`;
+            // Adiciona o valor paga
+            detalhesReserva += ` | Pago: PYG ${reserva.valorPago.toFixed(2)}`;
 
             const botaoExcluir = document.createElement('button');
             botaoExcluir.textContent = 'Excluir';
@@ -230,7 +250,8 @@ document.addEventListener('DOMContentLoaded', () => {
             salvarReservas();
             renderizarReservas();
             renderizarQuartos();
-            if (modalAdicionarReserva.style.display === 'block' && quartoReservaSelect.value) {
+            if (modalAdicionarReserva.style.display === 'block' && 
+                quartoReservaSelect.value) {
                 quartoReservaSelect.dispatchEvent(new Event('change'));
             }
         }
@@ -261,6 +282,60 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         if (event.target == modalAdicionarReserva) {
             modalAdicionarReserva.style.display = 'none';
+        }
+    });
+
+    const usuarios = [
+        { usuario: 'adm', senha: 'adm', tipo: 'admin' },
+        { usuario: 'user', senha: 'user', tipo: 'comum' }
+    ];
+
+    // Referências
+    const loginForm = document.getElementById('login-form');
+    const loginContainer = document.getElementById('login-container');
+    const erroLogin = document.getElementById('login-erro');
+    const body = document.body;
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const usuario = document.getElementById('usuario').value;
+        const senha = document.getElementById('senha').value;
+
+        const user = usuarios.find(u => u.usuario === 
+            usuario && u.senha === senha);
+        if (user) {
+            localStorage.setItem('usuarioLogado', JSON.stringify(user));
+            aplicarPermissoes(user.tipo);
+            loginContainer.style.display = 'none';
+        } else {
+            erroLogin.style.display = 'block';
+        }
+    });
+
+    function aplicarPermissoes(tipo) {
+        if (tipo === 'admin') {
+            document.body.classList.add('admin');
+        } else {
+            document.body.classList.remove('admin');
+            document.body.classList.add('comum');
+
+            // Esconde botões de adição
+            document.getElementById('adicionar-hospede-btn').style.display = 'none';
+            document.getElementById('adicionar-reserva-btn').style.display = 'none';
+
+            // Desabilita botões de exclusão se houver
+            document.querySelectorAll('.excluir-reserva-btn').
+                forEach(btn => btn.style.display = 'none');
+        }
+    }
+
+    // Carregar login salvo (se houver)
+    window.addEventListener('DOMContentLoaded', () => {
+        const usuarioSalvo = localStorage.getItem('usuarioLogado');
+        if (usuarioSalvo) {
+            const user = JSON.parse(usuarioSalvo);
+            aplicarPermissoes(user.tipo);
+            loginContainer.style.display = 'none';
         }
     });
 });
